0c158518be86eb7282a62e7df889ae024fa0c35d
Hoàng Phúc <139773908+WangChukz@users.noreply.github.com>
2024-02-12 23:24:14 +0700

    Update Find second max in array.c
    Signed-off-by: Hoàng Phúc <139773908+WangChukz@users.noreply.github.com>

--- Find second max in array.cpp
+++ Find second max in array.cpp
@@ -1,54 +1,54 @@
 #include <stdio.h>
-void nhap_mang(int a[], int n){
-	for(int i = 0; i < n; i++){
-      printf("Nhap vao phan tu a[%d]: ", i);
-      scanf("%d", &a[i]);			 
-   }
+void Input(int a[], int n) {
+	for(int i = 0; i < n; i++) {
+		printf("Nhap vao phan tu a[%d]: ", i);
+		scanf("%d", &a[i]);
+	}
 }
-int find_max(int a[], int n){
- 	int max = a[0];
-	for(int i = 1; i < n; i++){
-	if(a[i] > max){
-	   max = a[i];
-     }
-   }
-   return max;
-}
-int find_second_max(int a[], int n){
-	int max = find_max(a, n);
-	int max2 = max;
-	for(int i = 0; i < n; i++){
-		if(a[i] != max){
-        max2 = a[i];
-        break;
+int find_max(int a[], int n) {
+	int max = a[0];
+	for(int i = 1; i < n; i++) {
+		if(a[i] > max) {
+			max = a[i];
 		}
 	}
-	for(int i = 0; i < n; i++){
-		if(a[i] > max2 && a[i] != max){
-     	  max2 = a[i];
+	return max;
+}
+int find_second_max(int a[], int n) {
+	int max = find_max(a, n);
+	int max2 = max;
+	for(int i = 0; i < n; i++) {
+		if(a[i] != max) {
+			max2 = a[i];
+			break;
+		}
+	}
+	for(int i = 0; i < n; i++) {
+		if(a[i] > max2 && a[i] != max) {
+			max2 = a[i];
 		}
 	}
 	return max2;
 }
-int xuat_mang(int a[], int n){
+void Output(int a[], int n) {
 	int max = find_max(a, n);
 	int max2 = find_second_max(a, n);
-	if(max != max2){
-		printf("gia tri lon nhat trong mang la:%d", max);
-   	printf("\nSo lon thu hai trong mang la:%d", max2);
+	if(max != max2) {
+		printf("So lon nhat trong mang la:%d", max);
+		printf("\nSo lon thu hai trong mang la:%d", max2);
 	} else {
 		printf("Khong co gia tri lon thu nhat va lon thu hai trong mang!");
 	}
 }
-int main(){
+int main() {
 	int a[1000], n;
 	printf("Nhap phan tu n (n > 0): ");
 	scanf("%d", &n);
-	if(n < 0){
+	if(n < 0) {
 		printf("Gia tri n khong thoa man. Hay nhap lai!");
 	} else {
-		nhap_mang(a, n);
-      xuat_mang(a, n);
+		Input(a, n);
+		Output(a, n);
 	}
 	printf("\n----------------------------------\n");
 	main();
